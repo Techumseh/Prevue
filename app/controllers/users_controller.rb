d902bb6f15a765166a6f6a2db17a6e1ecd07fd4f
Jeff Bond <jeffreybond@mac.com>
2021-01-26 15:20:58 -0500

    more backend work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,3 +49,20 @@ class UsersController < ApplicationController
       params.require(:user).permit(:username, :email, :password_digest)
     end
 end
+
+def login
+  @user = User.find_by_username(params[:username])
+  if @user.authenticate(params[:password]) #authenticate method provided by Bcrypt and 'has_secure_password'
+    token = encode(user_id: @user.id, username: @user.username)
+    render json: { token: token }, status: :ok
+  else
+    render json: { error: 'unauthorized' }, status: :unauthorized
+  end
+end
+
+private
+
+def login_params
+  params.permit(:username, :password)
+end
+end
