9b3ca80b4d7956429686698759475c709f3fffc7
Jeff Bond <jeffreybond@mac.com>
2021-01-27 10:59:22 -0500

    corrected typos in Login.jsx

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export default function Login() {
-  return
-  <form>
-    <h3>Login</h3>
-    <label>Username:
-      <input />
-    </label>
-    <label>Password
+  return (
+    <form>
+      <h3>Login</h3>
+      <label>Username:
+        <input />
+      </label>
+      <br/>
+    <label>Password:
      <input />
     </label>
     <Link>Register</Link>
     <button>Submit</button>
-  </form>
+    </form>
+  )
 }
 
 export default Login;
