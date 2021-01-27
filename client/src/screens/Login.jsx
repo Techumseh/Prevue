b25122bf61cdd9956bb8f5e0f60b5020baa45fd9
Jeff Bond <jeffreybond@mac.com>
2021-01-27 11:07:12 -0500

    updated form data ob Login.jsx

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
 
 export default function Login() {
+  const [formData, setFormdata] = useState({
+    username: '';
+    password: ''
+  })
+  const { username, password } = formData;
+  
+  const hanfleChange = (e) => {
+    const { name, value } = e.target;
+    setFormdata(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
   return (
     <form>
       <h3>Login</h3>
       <label>Username:
         <input />
+        type='text'
+        name='username'
+        value={username}
+        onChange={}
       </label>
       <br/>
     <label>Password:
      <input />
+     type='passwod'
+        name='password'
+        value={password}
+        onChange={}
     </label>
     <Link>Register</Link>
     <button>Submit</button>
