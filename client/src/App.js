57f43be95a0cefb52b818826a5b84dc377e05bc0
Jeff Bond <jeffreybond@mac.com>
2021-01-27 10:40:08 -0500

    Created Login screen and Layouts

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
-
+import Layout from "./layouts/Layouts"
+import { Switch } from 'react-router-dom';
+import Login from './screens/Login.jsx'
 
 function App() {
   return (
     <Layout>
-      
+      <Switch>
+        <Route path='/login'>
+
+        </Route>
+      </Switch>
+
     </Layout>
   );
 }
---
+++ client/src/screens/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Login() {
+  return
+  <form>
+    <h3>Login</h3>
+    <label>Username:
+      <input />
+    </label>
+    <label>Password
+     <input />
+    </label>
+    <Link>Register</Link>
+    <button>Submit</button>
+  </form>
+}
+
+export default Login;
