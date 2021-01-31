c2b951aae3ee599b6b8c1a5d0fb1f05fbee180be
Jeff Bond <jeffreybond@mac.com>
2021-01-31 15:49:08 -0500

    EditComments.jsx

---
+++ client/src/screens/EditComments.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function EditComments(props) {
+  const [formData, setFormData] = useState({
+    name: ''
+  })
+  const { name } = formData;
+  const { comments, handleUpdate } = props;
+  const { id } = useParams();
+
+  useEffect(() => {
+    const prefillFormData = () => {
+      const commetItem = commentss.find((commentItem) => {
+        return foodItem.id === Number(id)
+      })
+      setFormData({
+        name: commentItem.name
+      })
+    }
+    if ( commentss.length) {
+      prefillFormData()
+    }
+  }, [comments])
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
+  return (
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      handleUpdate(id, formData);
+    }}>
+      <h3>Edit Comments</h3>
+      <label>Name:
+        <input
+          type='text'
+          name='name'
+          value={name}
+          onChange={handleChange}
+        />
+      </label>
+      <button>Submit</button>
+    </form>
+  )
+}
