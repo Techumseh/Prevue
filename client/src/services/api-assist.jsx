9ec885cd550c1a0d7c5dfc03b65842feab87555d
Jeff Bond <jeffreybond@mac.com>
2021-02-02 09:49:13 -0500

    Preparing for another attempt at deployment

--- client/src/services/api-assist.jsx
+++ client/src/services/api-assist.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = process.env.NODE_ENV === 'production' ? /* link to your heroku app. Example:*/'https://prevue-api.herokuapp.com/' : 'http://localhost:3000' 
+const baseUrl = process.env.NODE_ENV === 'bf8d5f99335d7877fd8292222abe29be' ? 'https://prevue-api.herokuapp.com/' : 'http://localhost:3000' 
 
 const api = axios.create({
   baseURL: baseUrl
